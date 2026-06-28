e72b9f3cfbe8feaa485ad68ebd6ad9ee8af5224a
ABHAY PRATAP CHAUHAN <abhaychauhan2906@gmail.com>
2026-06-28 17:36:12 +0530

    bug fixed

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -32,7 +32,7 @@ export default function Home() {
       console.error("failed to create project");
       return false;
     }
-    setProjects((prev)=>[newItem,...prev]);
+    setProjects((prev)=>[saved,...prev]);
     navigate(`/visualizer/${newId}`, {
       state: {
         initialImage: saved?.sourcePath,
